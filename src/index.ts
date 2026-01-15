d4af8f9cb8935a0a79c67843ae50a2d4121f33ac
DiqySH <margolang55@gmail.com>
2026-01-15 20:46:06 +0700

    add limit

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ dotenv.config();
 const PORT = Number(process.env.PORT) || 3333;
 
 const app = express();
+
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
 app.use(cors());
-app.use(express.json());
 
 app.get("/", (_: Request, res: Response) => {
   res.json({
@@ -17,7 +20,12 @@ app.get("/", (_: Request, res: Response) => {
   });
 });
 
-app.use("/api/ai", aiRoutes);
+app.use(
+  "/api/ai",
+  express.json({ limit: "20mb" }),
+  express.urlencoded({ extended: true, limit: "20mb" }),
+  aiRoutes
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
